a4d30a6bee12e0e56d0d509dc14fa2ebf4ef40f2
aleisley <anthon.asares@gmail.com>
2021-08-22 20:57:10 +0800

    add cords

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://3.0.214.160'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 
 @app.get("/")
